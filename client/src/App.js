f79210f4a0fce4bb123666b982e0bda9f845de9f
AinTisar <mrtozdmr92@gmail.com>
2021-05-30 20:35:07 +0300

    Reducer ayarlandı

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+
 import "./styles/style.scss";
 
 export default function App() {
@@ -25,4 +26,3 @@ export default function App() {
     </Router>
   );
 }
-
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,57 +1,13 @@
 import { React } from "react";
-import { createStore } from "redux";
-
-const incrementCount = ({ incrementBy = 1 } = {}) => ({
-  type: "INCREMENT",
-  incrementBy,
-});
-
-const decrementCount = ({ decrementBy = 1 } = {}) => ({
-  type: "DECREMENT",
-  decrementBy,
-});
-
-const setCount = ({ count = 0 } = {}) => ({
-  type: "SET",
-  count,
-});
-
-const resetCount = () => ({
-  type: "RESET",
-});
-
-const store = createStore((state = { count: 0 }, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return { count: state.count + action.incrementBy };
-    case "DECREMENT":
-      return { count: state.count - action.decrementBy };
-    case "RESET":
-      return { count: 0 };
-    case "SET":
-      return { count: action.count };
-    default:
-      return state;
-  }
-});
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(resetCount());
-
-store.dispatch(setCount({ count: 100 }));
-
-store.dispatch(incrementCount({ incrementBy: 50 }));
-
-store.dispatch(decrementCount({ decrementBy: 12 }));
+import Script from "../components/Script";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome</h1>
       <p>This is my site. Take a look</p>
+      <p>This is my site. Take a look</p>
+      <Script />
     </div>
   );
 }
